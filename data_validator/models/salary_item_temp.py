f56f669f0ada9195f8b27091f0d13ba52c649443
unknown <abeerhasan080@mail.com>
2024-07-21 10:17:14 +0300

    [MERGE]

--- data_validator/models/salary_item_temp.py
+++ data_validator/models/salary_item_temp.py
@@ -20,12 +20,58 @@ class SalaryItem(models.Model):
     # region  Basic
     need_confirm = fields.Boolean(default=False)
     fix_note = fields.Text()
+    date = fields.Date(string='Date', default=fields.Date.context_today)
+    transportation = fields.Monetary(string='Transportation', currency_field='currency_id')
+    mobile = fields.Monetary(string='Mobile', currency_field='currency_id')
+    location = fields.Char(string='Location')
+    meal = fields.Monetary(string='Meal', currency_field='currency_id')
+    plant = fields.Char(string='Plant')
+    parking = fields.Monetary(string='Parking', currency_field='currency_id')
+    fixed_ot = fields.Monetary(string='Fixed OT', currency_field='currency_id')
+    commission = fields.Monetary(string='Commission', currency_field='currency_id')
+    social_raise = fields.Monetary(string='Social Raise', currency_field='currency_id')
+    guaranteed_bonus = fields.Monetary(string='Guaranteed Bonus', currency_field='currency_id')
+    housing = fields.Monetary(string='Housing', currency_field='currency_id')
+    acting = fields.Monetary(string='Acting', currency_field='currency_id')
+    job_nature = fields.Char(string='Job Nature')
+    occasional_bonus = fields.Monetary(string='Occasional Bonus', currency_field='currency_id')
+    offshore = fields.Monetary(string='Offshore', currency_field='currency_id')
+
+    # Salary Calculation section
+    gross = fields.Monetary(string='Gross', currency_field='currency_id')
+    full_basic = fields.Monetary(string='Full Basic', currency_field='currency_id')
+    rate = fields.Float(string='Rate')
+    all_insured = fields.Monetary(string='All Insured', currency_field='currency_id')
+    all_not_insured = fields.Monetary(string='All Not Insured', currency_field='currency_id')
+    full_basic_si = fields.Monetary(string='Full Basic SI', currency_field='currency_id')
+    full_salary_si = fields.Monetary(string='Full Salary SI', currency_field='currency_id')
+
+    # Social Insurance Calculation section
+    fixed = fields.Monetary(string='Fixed')
+    si_amount = fields.Monetary(string='SI Amount')
+    si_salary = fields.Monetary(string='SI Salary')
+    bank_name = fields.Char(string='Bank Name')
+    fees_on = fields.Selection([
+        ('company', 'Company'),
+        ('employee', 'Employee'),
+        ('company_employee', 'Company & Employee'),
+    ], string='Fees On')
+    bank_fees = fields.Monetary(string='Bank Fees')
+    deduction_fees = fields.Monetary(string='Deduction Fees')
+
+    refund_salary = fields.Monetary(string='Refund Salary')
+
+    basic = fields.Monetary(string='Basic', currency_field='currency_id')
+    currency_id = fields.Many2one('res.currency', string='Currency')
+    pay_currency_id = fields.Many2one('res.currency', string='Pay Currency')
+    salary_structure_id = fields.Many2one('hr.payroll.structure', string='Salary Structure')
     # endregion
 
     # region  Special
     # endregion
 
     # region  Relational
+    contract_id = fields.Many2one('hr.contract', string='Contract')
     # endregion
 
     # region  Computed
